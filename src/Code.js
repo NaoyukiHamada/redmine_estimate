e4b46267f2a33517726ca51de24680492a959877
Naoyuki Hamada <hamadanaoyuki00@gmail.com>
2019-10-19 00:00:22 +0900

    Add Slackにメッセージ送信用関数作成

--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,28 @@
-function myFunction() {
-  
+/**
+ * Slackにメッセージを送信
+ *
+ * @param channelName チェンネル名 publicチャンネルの場合は#を頭につける
+ * @param userName ユーザー名
+ * @param iconName アイコン名 カスタム絵文字を利用可能 e.g. :memo:
+ * @param message メッセージ
+ */
+function sendMessageToSlack(channelName, userName, iconName, message) {
+    const slackWebHookUrl = 'https://hooks.slack.com/services/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';
+
+    const jsonData =
+        {
+            "channel": channelName,
+            "username": userName,
+            "icon_emoji": iconName,
+            "text": message
+        };
+    const payload = JSON.stringify(jsonData);
+
+    const options =
+        {
+            "method": "post",
+            "contentType": "application/json",
+            "payload": payload
+        };
+    UrlFetchApp.fetch(slackWebHookUrl, options);
 }
