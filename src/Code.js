4448126c9d8b5e30a27d6d62a91bb82c3bc3195f
Naoyuki Hamada <hamadanaoyuki00@gmail.com>
2019-10-19 06:07:49 +0900

    Add redmineから合計予定工数を算出してSlackへ通知

--- src/Code.js
+++ src/Code.js
@@ -1,11 +1,24 @@
+/**
+ * シート名用Enum
+ * @type {{MESSAGE_TEMPLATE: string, SLACK_SETTING: string}}
+ */
+//gasのバグによりconstでエラーになるので、varを使用
 var SheetName = {
     SLACK_SETTING: 'SlackSetting',
     MESSAGE_TEMPLATE: 'MessageTemplate'
 };
 
+/**
+ * SlackSettingシートのコンテンツの行始め
+ * @type {number}
+ */
 //gasのバグによりconstでエラーになるので、varを使用
 var SLACK_SETTING_SHEET_CONTENT_START_INDEX = 2;
 
+/**
+ * SlackSettingシートの各カラムEnum
+ * @type {{NOTIFICATION_ON_OFF: number, DUE_DATE: number, SLACK_CHANNEL_NAME: number, ICON_NAME: number, VERSION: number, ID: number, USER_NAME: number, MESSAGE_TEMPLATE_ID: number, RED_MINE_QUERY_ID: number, PROJECT_NAME: number}}
+ */
 //gasのバグによりconstでエラーになるので、varを使用
 var SlackSettingSheetColumn = {
     ID: 1,
@@ -16,13 +29,26 @@ var SlackSettingSheetColumn = {
     USER_NAME: 6,
     ICON_NAME: 7,
     MESSAGE_TEMPLATE_ID: 8,
-    NOTIFICATION_ON_OFF: 9
+    RED_MINE_QUERY_ID: 9,
+    NOTIFICATION_ON_OFF: 10
 };
 
+/**
+ * MessageTemplateシートの各カラムEnum
+ * @type {{MESSAGE: number, ID: number}}
+ */
+//gasのバグによりconstでエラーになるので、varを使用
 var MessageTemplateSheetColumn = {
     ID: 1,
     MESSAGE: 2
-}
+};
+
+/**
+ * 1日の実働時間の基準
+ * @type {number} 時間
+ */
+//gasのバグによりconstでエラーになるので、varを使用
+var ACTUAL_WORKING_HOURS = 7;
 
 /**
  * Slackにメッセージを送信
@@ -33,7 +59,7 @@ var MessageTemplateSheetColumn = {
  * @param message メッセージ
  */
 function sendMessageToSlack(channelName, userName, iconName, message) {
-    const slackWebHookUrl = 'https://hooks.slack.com/services/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';
+    const slackWebHookUrl = 'https://hooks.slack.com/services/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';
 
     const jsonData =
         {
@@ -70,8 +96,9 @@ function readSlackSettingAndSendToSlack() {
         const userName = slackSettingSheet.getRange(i, SlackSettingSheetColumn.USER_NAME).getValue();
         const iconName = slackSettingSheet.getRange(i, SlackSettingSheetColumn.ICON_NAME).getValue();
         const messageTemplateId = slackSettingSheet.getRange(i, SlackSettingSheetColumn.MESSAGE_TEMPLATE_ID).getValue();
+        const redMineQueryId = slackSettingSheet.getRange(i, SlackSettingSheetColumn.RED_MINE_QUERY_ID).getValue();
         const shouldSendToSlack = slackSettingSheet.getRange(i, SlackSettingSheetColumn.NOTIFICATION_ON_OFF).getValue();
-        const message = createMessage(spreadsheet, messageTemplateId, version, dueDate);
+        const message = createMessage(spreadsheet, messageTemplateId, version, dueDate, redMineQueryId);
         if (shouldSendToSlack) {
             sendMessageToSlack(slackChannelName, userName, iconName, message);
         }
@@ -84,15 +111,16 @@ function readSlackSettingAndSendToSlack() {
  * @param messageTemplateId
  * @param version
  * @param dueDate
+ * @param redMineQueryId
  * @returns {string}
  */
-function createMessage(spreadsheet, messageTemplateId, version, dueDate) {
+function createMessage(spreadsheet, messageTemplateId, version, dueDate, redMineQueryId) {
     const messageTemplateSheet = spreadsheet.getSheetByName(SheetName.MESSAGE_TEMPLATE);
     const row = findRow(messageTemplateSheet, messageTemplateId, MessageTemplateSheetColumn.ID);
     var message = messageTemplateSheet.getRange(row, MessageTemplateSheetColumn.MESSAGE).getValue();
     switch (messageTemplateId) {
         case 1: {
-            message = createEstimateReportMessage(version, dueDate, message);
+            message = createEstimateReportMessage(version, dueDate, message, redMineQueryId);
             break;
         }
         default : {
@@ -107,10 +135,14 @@ function createMessage(spreadsheet, messageTemplateId, version, dueDate) {
  * @param version
  * @param dueDate
  * @param message
+ * @param redMineQueryId
  */
-function createEstimateReportMessage(version, dueDate, message) {
+function createEstimateReportMessage(version, dueDate, message, redMineQueryId) {
     const formattedDueDate = Utilities.formatDate(dueDate, 'JST', 'yyyy/MM/dd');
-    return Utilities.formatString(message, version, 23, formattedDueDate, 30, 7, "3日超過", "2日超過", "1日超過", "1日余剰", "2日余剰");
+    const totalEstimateTime = getTotalEstimateTimeFromRedMine(redMineQueryId);
+    //人日
+    const manDay = totalEstimateTime / ACTUAL_WORKING_HOURS;
+    return Utilities.formatString(message, version, 23, formattedDueDate, manDay, ACTUAL_WORKING_HOURS, "3日超過", "2日超過", "1日超過", "1日余剰", "2日余剰");
 }
 
 /**
@@ -129,6 +161,47 @@ function findRow(sheet, val, col) {
         }
     }
     return -1;
+}
+
+/**
+ * Redmineから指定のカスタムクエリの全チケットの合計予定工数を算出
+ * チケットの進捗率に応じて合計予定工数を削減
+ * RedmineAPIの制限で一度に取得できるチケットは100件まで
+ *
+ * @param redMineQueryId redmineで作成したカスタムクエリのID(カスタムクエリを開いた時のURLに記載)
+ * @returns {number} 合計予定工数
+ */
+function getTotalEstimateTimeFromRedMine(redMineQueryId) {
+    const baseUrl = 'https://www2195ue.sakura.ne.jp/redmine/';
+    const path = 'issues.json';
+    const token = PropertiesService.getScriptProperties().getProperty('red_mine_token');
+    const limit = 100;
+    //何ページ読み込むべきかを算出
+    const requestUrlForIssueTotalCount = baseUrl + path + '?query_id=' + redMineQueryId + '&key=' + token;
+    const responseJsonForIssueTotalCount = UrlFetchApp.fetch(requestUrlForIssueTotalCount).getContentText();
+    const responseMapForIssueTotalCount = JSON.parse(responseJsonForIssueTotalCount);
+    const totalCount = responseMapForIssueTotalCount.total_count;
+    var pageCount;
+    if (totalCount < 100) {
+        pageCount = 1;
+    } else {
+        pageCount = Math.ceil(totalCount / limit);
+    }
+
+    //チケット全ての合計予定工数を算出
+    var totalEstimate = 0;
+    for (var i = 1; i <= pageCount; i++) {
+        var requestUrl = baseUrl + path + '?query_id=' + redMineQueryId + '&key=' + token + '&limit=' + limit + '&page=' + i;
+        var responseJson = UrlFetchApp.fetch(requestUrl).getContentText();
+        var responseMap = JSON.parse(responseJson);
+        responseMap.issues.forEach(function (issue) {
+            var estimateTime = issue.estimated_hours || 0;
+            //現在の進捗率に応じて予定工数を削減
+            var estimateTimeByProgress = estimateTime * (100 - issue.done_ratio) / 100;
+            totalEstimate += estimateTimeByProgress;
+        });
+    }
+    return totalEstimate
 }
 
 
