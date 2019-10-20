976863a7e33b918dead9e0e4cdd53c2250a25bd0
Naoyuki Hamada <hamadanaoyuki00@gmail.com>
2019-10-20 23:52:43 +0900

    Fix tokenが切れたwebhook urlを変更

--- src/Code.js
+++ src/Code.js
@@ -83,7 +83,7 @@ var HOLIDAY_SHEET_CONTENT_START_INDEX = 2;
  */
 function sendMessageToSlack(channelName, userName, iconName, title, message, isTest) {
     // const slackSendMessageUrl = 'https://slack.com/api/chat.postMessage';
-    const slackSendMessageUrl = 'https://hooks.slack.com/services/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';
+    const slackSendMessageUrl = 'https://hooks.slack.com/services/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';
 
     if (title != null && title.length > 0) {
         message = '*' + title + '*\n\n' + message;
